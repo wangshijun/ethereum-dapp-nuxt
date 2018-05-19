e68cada8c4206f98dab3ab62922386febc99c9b0
wangshijun <wangshijun2010@gmail.com>
2018-05-20 07:24:27 +0800

    compile script: cleanup before build

--- scripts/compile.js
+++ scripts/compile.js
@@ -2,15 +2,21 @@ const fs = require('fs-extra');
 const path = require('path');
 const solc = require('solc');
 
+// cleanup
+const compiledDir = path.resolve(__dirname, '../compiled');
+fs.removeSync(compiledDir);
+fs.ensureDirSync(compiledDir);
+
+// compile
 const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol');
 const contractSource = fs.readFileSync(contractPath, 'utf8');
-
 const result = solc.compile(contractSource, 1);
 console.log(result);
 
+// save to disk
 Object.keys(result.contracts).forEach(name => {
     const contractName = name.replace(/^:/, '');
-    const filePath = path.resolve(__dirname, '../compiled', `${contractName}.json`);
+    const filePath = path.resolve(compiledDir, `${contractName}.json`);
     fs.outputJsonSync(filePath, result.contracts[name]);
     console.log(`save compiled contract ${contractName} to ${filePath}`);
 });
