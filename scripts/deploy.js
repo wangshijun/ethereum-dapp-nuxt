3dee2ca5d96dfe97d90ce9e123aacd3c0e995219
wangshijun <wangshijun2010@gmail.com>
2018-05-22 07:01:32 +0800

    record contract deploy time

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -21,9 +21,11 @@ const web3 = new Web3(provider);
     console.log('部署合约的账户：', accounts[0]);
 
     // 5. 创建合约实例并且部署
+    console.time('contract-deploy');
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['AUDI'] })
         .send({ from: accounts[0], gas: '1000000' });
+    console.timeEnd('contract-deploy');
 
-    console.log('合约部署成功：', result);
+    console.log('合约部署成功：', result.options.address);
 })();
