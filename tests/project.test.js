831f683db0c6311356679f9d615c3b57b413246e
wangshijun <wangshijun2010@gmail.com>
2018-05-27 06:28:49 +0800

    project test: contribute

--- tests/project.test.js
+++ tests/project.test.js
@@ -53,4 +53,15 @@ describe.only('Project Contract', () => {
         assert.equal(maxInvest, 10000);
         assert.equal(goal, 1000000);
     });
+
+    it('should allow investor to contribute', async () => {
+        const investor = accounts[1];
+        await project.methods.contribute().send({
+            from: investor,
+            value: '200',
+        });
+
+        const amount = await project.methods.investors(investor).call();
+        assert.ok(amount == '200');
+    });
 });
