1f0d76d19b2d440c3d8f385b4df762972af173fa
wangshijun <wangshijun2010@gmail.com>
2018-05-29 07:09:22 +0800

    use layout component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+
 import withRoot from '../libs/withRoot';
+import Layout from '../components/Layout';
 
 class Index extends React.Component {
   render() {
-    return <Button variant="raised" color="primary">Welcome to Ethereum ICO DApp!</Button>;
+    return (
+      <Layout>
+        <Button variant="raised" color="primary">
+          Welcome to Ethereum ICO DApp!
+        </Button>
+      </Layout>
+    );
   }
 }
 
