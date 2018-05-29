738739a3075e8c0d50ee1222a3c43f4576e2ff86
wangshijun <wangshijun2010@gmail.com>
2018-05-29 10:34:07 +0800

    render blockchain data in dapp

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import Web3 from 'web3';
 import { Button } from '@material-ui/core';
 
 import withRoot from '../libs/withRoot';
 import Layout from '../components/Layout';
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accounts: [],
+    };
+  }
+
+  async componentDidMount() {
+    const web3 = new Web3(window.web3.currentProvider);
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+
+    this.setState({ accounts });
+  }
+
   render() {
+    const { accounts } = this.state;
+
     return (
       <Layout>
-        <Button variant="raised" color="primary">
-          Welcome to Ethereum ICO DApp!
-        </Button>
+        <ul>{accounts.map(x => <li key={x}>{x}</li>)}</ul>
       </Layout>
     );
   }
