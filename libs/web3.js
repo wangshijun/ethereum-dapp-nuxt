f622dc0ec8d896c53b16287cc531ae5f216ab210
wangshijun <wangshijun2010@gmail.com>
2018-05-31 07:20:59 +0800

    use next.js config

--- libs/web3.js
+++ libs/web3.js
@@ -1,13 +1,16 @@
 import Web3 from 'web3';
+import getConfig from 'next/config';
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 let web3;
 // 浏览器环境且已经安装了 Metamask
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 
-// 服务器环境或者没有安装 Metamask
+  // 服务器环境或者没有安装 Metamask
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/CqCd0QgCozHBEk19ub2M'));
+  web3 = new Web3(new Web3.providers.HttpProvider(publicRuntimeConfig.infuraUrl));
 }
 
 export default web3;
