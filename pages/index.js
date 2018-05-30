3e1208ed608da5e3f5262a1f3741df54c0815b12
wangshijun <wangshijun2010@gmail.com>
2018-05-30 09:35:10 +0800

    render project description

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,42 @@ import { Grid, Button, Typography, Card, CardContent, CardActions } from '@mater
 
 import { Link } from '../routes';
 import web3 from '../libs/web3';
+import Project from '../libs/project';
 import ProjectList from '../libs/projectList';
 import withRoot from '../libs/withRoot';
 import Layout from '../components/Layout';
 
 class Index extends React.Component {
   static async getInitialProps({ req }) {
-    const projects = await ProjectList.methods.getProjects().call();
+    const addressList = await ProjectList.methods.getProjects().call();
+    const summaryList = await Promise.all(
+      addressList.map(address =>
+        Project(address)
+          .methods.getSummary()
+          .call()
+      )
+    );
+    console.log({ summaryList });
+    const projects = addressList.map((address, i) => {
+      const [description, minInvest, maxInvest, goal, balance, investorCount, paymentCount, owner] = Object.values(
+        summaryList[i]
+      );
+
+      return {
+        address,
+        description,
+        minInvest,
+        maxInvest,
+        goal,
+        balance,
+        investorCount,
+        paymentCount,
+        owner,
+      };
+    });
+
+    console.log(projects);
+
     return { projects };
   }
 
@@ -27,21 +56,21 @@ class Index extends React.Component {
 
   renderProject(project) {
     return (
-      <Grid item md={4} key={project}>
+      <Grid item md={4} key={project.address}>
         <Card>
           <CardContent>
             <Typography gutterBottom variant="headline" component="h2">
-              {project}
+              {project.description}
             </Typography>
-            <Typography component="p">{project}</Typography>
+            <Typography component="p">{project.address}</Typography>
           </CardContent>
           <CardActions>
-            <Link route={`/projects/${project}`}>
+            <Link route={`/projects/${project.address}`}>
               <Button size="small" color="primary">
                 立即投资
               </Button>
             </Link>
-            <Link route={`/projects/${project}`}>
+            <Link route={`/projects/${project.address}`}>
               <Button size="small" color="secondary">
                 查看详情
               </Button>
