55234b98c7539d5089bd2a2b953009a776816c0b
wangshijun <wangshijun2010@gmail.com>
2018-05-31 06:52:03 +0800

    initial config

---
+++ config/default.js
@@ -0,0 +1,4 @@
+module.exports = {
+  infuraUrl: 'https://rinkeby.infura.io/CqCd0QgCozHBEk19ub2M',
+  hdwallet: 'witness improve busy opinion addict sun gossip hedgehog common glass dignity primary',
+};
