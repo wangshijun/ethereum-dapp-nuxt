f7d358d6ea04f0544b88be7e0412f3bafbc7d9cb
wangshijun <wangshijun2010@gmail.com>
2018-06-01 09:25:40 +0800

    basic project list page

--- libs/web3.js
+++ libs/web3.js
@@ -1,7 +1,4 @@
 import Web3 from 'web3';
-import getConfig from 'next/config';
-
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 let web3;
 // 浏览器环境且已经安装了 Metamask
@@ -10,7 +7,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 
   // 服务器环境或者没有安装 Metamask
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider(publicRuntimeConfig.infuraUrl));
+  web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/CqCd0QgCozHBEk19ub2M'));
 }
 
 export default web3;
